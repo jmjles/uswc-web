c9502e37a39c2b72ed4471a11f2b9528899d2d51
jmjles <jmjles@gmail.com>
2021-01-15 16:56:30 -0800

    Added live dev link

--- util/axios.js
+++ util/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
+const dev = "https://us-weed-channel-server.herokuapp.com/vimeo";
+const prod = "https://api.usweedchannel.com/vimeo";
 
-const vimeoURL = "https://api.usweedchannel.com/vimeo";
+const vimeoURL = dev
 const devURL = "http://localhost:5000/vimeo";
+
 export const vimeo = axios.create({
   baseURL: process.env.NODE_ENV === "development" ? devURL : vimeoURL,
 });

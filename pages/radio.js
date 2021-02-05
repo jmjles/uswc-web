26009862f508d5f6680f174dee35941deb0aafd2
jmjles <jmjles@gmail.com>
2021-02-04 17:08:09 -0800

    Added new radio

--- pages/radio.js
+++ pages/radio.js
@@ -1,42 +1,42 @@
 import { Typography as Font } from "@material-ui/core";
 import Page from "../layout/Page";
-import Wip from "../components/Wip";
+import "video.js/dist/video-js.css";
 import Head from "next/head";
+
 const Radio = () => {
   return (
     <Page>
-      <Head>
-        
-      </Head>
+      <Head></Head>
       <section className="Radio">
         <a
           href="https://epsilon.shoutca.st:2199/tunein/usweedchannel.pls"
-          class="cc_streaminfo"
+          className="cc_streaminfo"
           data-type="song"
           data-username="usweedchannel"
         >
           <Font variant="h1">Loading ...</Font>
         </a>
         <img
-          class="cc_streaminfo"
+          className="cc_streaminfo"
           data-type="trackimageurl"
           data-username="usweedchannel"
         />
         <br />
-        <div class="cc_player" data-username="usweedchannel">
-          Loading ...
-        </div>
+        <audio controls={true}>
+          <source src="https://usweedchannel.radioca.st/stream" />
+        </audio>
+
       </section>
       <script
-          language="javascript"
-          type="text/javascript"
-          src="https://epsilon.shoutca.st:2199/system/player.js"
-        />
-        <script
-          language="javascript"
-          type="text/javascript"
-          src="https://epsilon.shoutca.st:2199/system/streaminfo.js"
-        />
+        language="javascript"
+        type="text/javascript"
+        src="https://epsilon.shoutca.st:2199/system/player.js"
+      />
+      <script
+        language="javascript"
+        type="text/javascript"
+        src="https://epsilon.shoutca.st:2199/system/streaminfo.js"
+      />
     </Page>
   );
 };

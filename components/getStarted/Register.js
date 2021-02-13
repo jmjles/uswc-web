1291ccaad35d4e08d50eac307e64ddc4367b8b2a
jmjles <jmjles@gmail.com>
2021-02-12 17:14:39 -0800

    added login and register form // started get starteed page // Removed Depreciated code

---
+++ components/getStarted/Register.js
@@ -0,0 +1,144 @@
+import { Button, TextField, Typography as Font } from "@material-ui/core";
+import { useState } from "react";
+import { server } from "../../util/axios";
+
+const Register = ({ style }) => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirm, setConfirm] = useState("");
+  const [tel, setTel] = useState("");
+  const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+      case "firstName":
+        setFirstName(value);
+        break;
+      case "lastName":
+        setLastName(value);
+        break;
+      case "email":
+        setEmail(value);
+        break;
+      case "username":
+        setUsername(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+      case "confirm":
+        setConfirm(value);
+        break;
+      case "tel":
+        setTel(value);
+        break;
+      default:
+        console.log("Unknown value");
+    }
+    console.log(name, value);
+  };
+
+  const handleSubmit = async () => {
+    if (password === confirm) {
+      try {
+        const res = await server.post("/user/register", {
+          firstName,
+          lastName,
+          email,
+          password,
+          tel,
+        });
+        console.log(res);
+      } catch (er) {
+        console.log(er.response);
+      }
+    }
+  };
+  return (
+    <form className="Register" style={style && style} onSubmit={handleSubmit}>
+      <TextField
+        name="firstName"
+        type="text"
+        required
+        placeholder="First Name"
+        variant="standard"
+        color="primary"
+        variant="standard"
+        onChange={handleChange}
+        value={firstName}
+      />
+      <TextField
+        name="lastName"
+        type="text"
+        required
+        placeholder="Last Name"
+        variant="standard"
+        color="primary"
+        variant="standard"
+        onChange={handleChange}
+        value={lastName}
+      />
+      <TextField
+        name="email"
+        type="text"
+        required
+        placeholder="Email"
+        variant="standard"
+        color="primary"
+        variant="standard"
+        onChange={handleChange}
+        value={email}
+      />
+      <TextField
+        name="username"
+        type="text"
+        required
+        placeholder="Username"
+        variant="standard"
+        color="primary"
+        variant="standard"
+        onChange={handleChange}
+        value={username}
+      />
+      <TextField
+        name="password"
+        type="text"
+        required
+        placeholder="Password"
+        variant="standard"
+        color="primary"
+        variant="standard"
+        onChange={handleChange}
+        value={password}
+      />
+      <TextField
+        name="confirm"
+        type="text"
+        required
+        placeholder="Confirm Password"
+        variant="standard"
+        color="primary"
+        variant="standard"
+        onChange={handleChange}
+        value={confirm}
+      />
+      <TextField
+        name="tel"
+        type="tel"
+        required
+        placeholder="Phone #"
+        variant="standard"
+        color="primary"
+        variant="standard"
+        onChange={handleChange}
+        value={tel}
+      />
+      <Button type="submit" variant="contained" color="primary">
+        <Font variant="button">Submit</Font>
+      </Button>
+    </form>
+  );
+};
+
+export default Register;
--- pages/get-started.js
+++ pages/get-started.js
@@ -1,12 +1,42 @@
+import { Button, createStyles, Typography as Font } from "@material-ui/core";
+import { useState } from "react";
+import Login from "../components/getStarted/Login";
+import Register from "../components/getStarted/Register";
 import Wip from "../components/Wip";
 import Page from "../layout/Page";
 
 const getStarted = () => {
-    return (
-        <Page className="GetStarted" title="Get Started">
-            <Wip/>
-        </Page>
-    );
-}
-
+  const [type, setType] = useState("none");
+  const handleType = (type) => setType(type);
+  return (
+    <Page className="GetStarted" title="Get Started">
+      <div style={type !== "none" ? styles.hidden : {}}>
+        <Font variant="h1">Get started with U.S. Weed Channel</Font>
+        <Button
+          variant="contained"
+          target="register"
+          onClick={() => handleType("register")}
+          color="primary"
+        >
+          <Font variant="button">Register</Font>
+        </Button>
+        <Button
+          variant="contained"
+          target="login"
+          onClick={() => handleType("login")}
+          color="primary"
+        >
+          <Font variant="button">Login</Font>
+        </Button>
+      </div>
+      <Register style={type !== "register" ? styles.hidden : {}} />
+      <Login style={type !== "login" ? styles.hidden : {}} />
+    </Page>
+  );
+};
+const styles = createStyles({
+  hidden: {
+    display: "none",
+  },
+});
 export default getStarted;

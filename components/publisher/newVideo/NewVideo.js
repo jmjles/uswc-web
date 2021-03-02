6e4007979dea653c88660345068b6a85b79420b4
jmjles <jmjles@gmail.com>
2021-03-02 13:07:40 -0800

    Added publisher and login  func

---
+++ components/publisher/newVideo/NewVideo.js
@@ -0,0 +1,11 @@
+import { TextField } from "@material-ui/core"
+
+const NewVideo = () => {
+    return (
+        <form>
+            <TextField/>
+        </form>
+    );
+}
+
+export default NewVideo;
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,5 +1,7 @@
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 import { useEffect } from "react";
+import NewVideo from "../components/publisher/newVideo/NewVideo";
+import Page from "../layout/Page";
 
 const dashboard = () => {
   const history = useRouter();
@@ -7,7 +9,11 @@ const dashboard = () => {
     const token = localStorage.getItem("token");
     if (!token) history.push("/get-started");
   }, []);
-  return <div>Enter</div>;
+  return (
+    <Page title="Dashboard" className="Dashboard">
+      <NewVideo />
+    </Page>
+  );
 };
 
 export default dashboard;
